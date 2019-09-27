7cd712e9574bb5c56bd764804e347617f74e5970
Simon Hudon <simon.hudon@gmail.com>
2019-09-27 19:27:07 -0400

    stdout

--- src/check.lean
+++ src/check.lean
@@ -1,11 +1,18 @@
 
 import system.io
 
+def io.cmd' (args : io.process.spawn_args) : io unit :=
+do child ← io.proc.spawn args,
+  io.fs.close child.stdout,
+  exitv ← io.proc.wait child,
+  when (exitv ≠ 0) $ io.fail $ "process exited with status " ++ repr exitv,
+  return ()
+
 open io io.proc io.fs
 
 def git_clone (repo : string) : io unit :=
 do put_str_ln sformat!"clone {repo}",
-   io.cmd { cmd := "git", args := ["clone",repo] } >>= put_str_ln
+   io.cmd' { cmd := "git", args := ["clone",repo] }
 
 def read_lines (fn : string) : io (list string) :=
 do h ← mk_file_handle fn io.mode.read ff,
