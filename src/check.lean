f088ec3996ce56c7a493972b64b66b080f24a691
Simon Hudon <simon.hudon@gmail.com>
2019-10-27 20:06:54 -0400

    cwd (2)

--- src/check.lean
+++ src/check.lean
@@ -350,6 +350,8 @@ def checkout_snapshot' (args : app_args) :
                when (¬ ex) $ git_clone p.url.head dir,
                git_fetch,
                env.set_cwd dir,
+               -- put_str_ln $ repr
+               put_str_ln sformat!"> cwd (2): {d}",
                match p.commit with
                | some sha := git_checkout_hash sha
                | none     := catch (git_checkout_tag args.tag) (λ _ : io.error, (return () : io unit))
