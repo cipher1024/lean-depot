6b7030f83a6c659003a1874f63ccf5e581716715
Simon Hudon <simon.hudon@gmail.com>
2019-10-28 20:08:41 -0400

    use stdout in `detect_errors.py`

--- scripts/detect_errors.py
+++ scripts/detect_errors.py
@@ -2,8 +2,8 @@ import itertools
 import sys
 
 for line in sys.stdin:
-    sys.stderr.write(line)
+    sys.stdout.write(line)
     if 'error' in line:
         for line in itertools.islice(sys.stdin, 20):
-            sys.stderr.write(line)
+            sys.stdout.write(line)
         sys.exit(1)
